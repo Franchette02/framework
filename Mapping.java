a9b86d752d7abcadf40061d410af63b20301e596
Franchette02 <franchmalalatina@gmail.com>
2023-03-31 07:43:59 +0300

    sprint3

--- Mapping.java
+++ Mapping.java
@@ -3,4 +3,18 @@ public class Mapping{
     String className;
     String method;
 
+ public String getClassName() {
+    return className;
+  }
+  public void setClassName(String className) {
+    this.className = className;
+  }
+
+   public String getMethod() {
+    return method;
+  }
+  public void setMethod(String method) {
+    method = method;
+  }
+
 }
--- Util.java
+++ Util.java
@@ -1,7 +1,28 @@
 package util;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Util{
     public String getMethode(String url){
         String[] meth=url.split("/");
         return meth[2];
     }
+
+
+    public List<Class<?>> getClassePackage(String packageName) throws Exception {
+    List<Class<?>> classes = new ArrayList<>();
+    ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+    String path = packageName.replace('.', '/');
+    for (java.net.URL resource : java.util.Collections.list(classLoader.getResources(path))) {
+        for (String file : new java.io.File(resource.toURI()).list()) {
+            if (file.endsWith(".class")) {
+                String className = packageName + '.' + file.substring(0, file.length() - 6);
+                Class<?> cl = Class.forName(className);
+                classes.add(cl);
+            }
+        }
+    }
+    return classes;
+}
+
 }
